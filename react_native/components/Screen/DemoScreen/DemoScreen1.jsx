c868eeb4ac1f22a460117e1b4a9583a4442f696f
273* <keita.y310@icloud.com>
2023-10-09 13:40:49 +0900

    拡張子を.jsxに変更しました．

--- react_native/components/Screen/DemoScreen/DemoScreen1.jsx
+++ react_native/components/Screen/DemoScreen/DemoScreen1.jsx
@@ -6,16 +6,11 @@ import { View, Text, Button, StyleSheet } from "react-native";
 
 // コンポーネント関数を定義する．
 function DemoScreen1() {
-
-    // 画面間を移動する場合はuseNavigation()フックを使ってnavigationオブジェクトを取りだす．
+  // 画面間を移動する場合はuseNavigation()フックを使ってnavigationオブジェクトを取りだす．
   const navigation = useNavigation();
 
   return (
-    <View
-      style={
-        styles.container
-      }
-    >
+    <View style={styles.container}>
       <Text style={styles.text}>Demo Screen1</Text>
       <Button
         title="Demo Screen2へ"
@@ -29,7 +24,7 @@ function DemoScreen1() {
       />
     </View>
   );
-};
+}
 
 // スタイル用の関数を定義する．
 // 呼び出しは上の通り
@@ -40,7 +35,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   text: {
-    color:"rgb(128 0 128)"
+    color: "rgb(128 0 128)",
   },
 });
 
