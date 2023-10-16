3400989866d963bf00b92bfeec9e95d2b829c585
haruyuki0525 <a601021106@st.tachibana-u.ac.jp>
2023-10-16 23:10:38 +0900

    カマキリ

--- react_native/components/Screen/ProfileScreen/ProfileScreen_Setting_User.jsx
+++ react_native/components/Screen/ProfileScreen/ProfileScreen_Setting_User.jsx
@@ -1,15 +1,40 @@
 import React from "react";
-import { View, Text, Button, Image } from "react-native";
+import { View, Text, Button, Image, StyleSheet } from "react-native";
 
 const ProfileScreen_Setting_User = ({ navigation }) => {
   // console.log(route);
   return (
-    <View>
+    <View style={styles.container}>
       <View>
-        <Image source={require("../../../assets/icon.png")} />
+        <Image
+          style={styles.headerImage}
+          source={require("../../../assets/icon.png")}
+        />
+        <Image
+          style={styles.iconImage}
+          source={require("../../../assets/icon.png")}
+        />
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+
+  headerImage: {
+    height: 100,
+    width: "100%",
+  },
+
+  iconImage: {
+    height: 70,
+    width: 70,
+  },
+});
 
 export default ProfileScreen_Setting_User;
